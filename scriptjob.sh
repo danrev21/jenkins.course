e2867d715f4e7c3d2841cf9865847f5b0d5c0c27
danrev21 <dan.rev2021@gmail.com>
2022-03-19 22:53:34 +0300

    tar 3

--- scriptjob.sh
+++ scriptjob.sh
@@ -4,6 +4,6 @@
 # echo "Name of the branch: $BRANCH_NAME"
 # echo "Build number: $BUILD_NUMBER"
 
-find ~/ -name *.txt > files_list
+find . -name *.txt > files_list
 tar -czvf example.tar.gz -T files_list
 rm files_list
