caa6ee9defda57f99d33ccbb82ea5002dcbbca80
danrev21 <dan.rev2021@gmail.com>
2022-03-19 21:46:58 +0300

    tar 1

--- bash.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Job name: $JOB_NAME"
-echo "Name of the branch: $BRANCH_NAME"
-echo "Build number: $BUILD_NUMBER"
---
+++ scriptjob.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# echo "Job name: $JOB_NAME"
+# echo "Name of the branch: $BRANCH_NAME"
+# echo "Build number: $BUILD_NUMBER"
+
+find ~/ -name *.txt > files_list
+tar -czvf example.tar.gz -T files_list
+rm files_list
