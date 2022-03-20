d9bb84fbcbeb082e59291ee5d285531ab334aa78
danrev21 <90831326+danrev21@users.noreply.github.com>
2022-03-20 10:53:07 +0300

    Update scriptjob.sh

--- scriptjob.sh
+++ scriptjob.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# echo "Job name: $JOB_NAME"
-# echo "Build number: $BUILD_NUMBER"
-# echo "Git URL: $GIT_URL"
+echo "Job name: $JOB_NAME"
+echo "Build number: $BUILD_NUMBER"
+echo "Git URL: $GIT_URL"
 
 find . -name '*.txt' > files_list
 tar -czvf jobarchive.tar.gz files_list > /dev/null 2>&1
 rm files_list 
 a=$(ls -la | grep jobarchive.tar.gz)
-# echo "Job artifact: $a"
+echo "Job artifact: $a"
