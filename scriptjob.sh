f212f837e5c138fd7978e4c6032d01c6a226f8ee
danrev21 <90831326+danrev21@users.noreply.github.com>
2022-03-20 22:30:03 +0300

    Update scriptjob.sh

--- scriptjob.sh
+++ scriptjob.sh
@@ -9,7 +9,7 @@ echo "------------------------------------------"
 mkdir $JENKINS_HOME/artefacts > /dev/null 2>&1
 rm -rf $JENKINS_HOME/artefacts/*.tar.gz > /dev/null 2>&1
 tar -czvf build_$BUILD_NUMBER.tar.gz UPLOADED_FILE > /dev/null 2>&1
-sudo chmod 755 build_$BUILD_NUMBER.tar.gz
 cp build_$BUILD_NUMBER.tar.gz $JENKINS_HOME/artefacts
+sudo chmod 755 $JENKINS_HOME/artefacts/build_$BUILD_NUMBER.tar.gz
 rm -rf * > /dev/null 2>&1
 ls $JENKINS_HOME/artefacts
