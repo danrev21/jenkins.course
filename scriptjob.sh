b7ae1ce9eb7eaf74b359b9486c108a48b7215c92
danrev21 <90831326+danrev21@users.noreply.github.com>
2022-03-20 21:12:22 +0300

    Update scriptjob.sh

--- scriptjob.sh
+++ scriptjob.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+echo "------------------------------------------"
 echo "Job name: $JOB_NAME"
 echo "Build number: $BUILD_NUMBER"
 echo "Jenkins dir: $JENKINS_HOME"
-
+echo "------------------------------------------"
 mkdir $JENKINS_HOME/artefacts > /dev/null 2>&1
 tar -czvf build_$BUILD_NUMBER.tar.gz UPLOADED_FILE
 cp build_$BUILD_NUMBER.tar.gz $JENKINS_HOME/artefacts
